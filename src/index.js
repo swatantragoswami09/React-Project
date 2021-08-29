ffc7659810d05c1fc39b4b05fea6e50fa7611628
swatantra.goswami <swatantra.goswami_cs17@gla.ac.in>
2021-08-29 18:10:04 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -39,14 +39,16 @@ class App extends Component{
   render(){
     const videoSearch=_.debounce((term)=>{this.videoSearch(term)},300);
     return (
-         <div>
-           <SearchBar onSearchTermChange={videoSearch} />
+
+         <div >
+           <SearchBar  onSearchTermChange={videoSearch} />
            <VideoDetail video={this.state.selectedVideo} />
            <VideoList
                 onVideoSelect={selectedVideo=>this.setState({selectedVideo}) }
                 videos={this.state.videos} />
 
          </div>
+         
     );
   }
 }
